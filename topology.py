c5da3d13b6057c14d92444adaba78adc011b8915
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-02 14:53:37 +0700

    Add files via upload

--- topology.py
+++ topology.py
@@ -1,17 +1,15 @@
-from itertools import takewhile
-
 objects = {"hA": 0, "hB": 1, "ss": 1, "ws": 0, "hosp": 0, "manuf": 0}  # кол-во объектов
-percents = {i: round(((20 * i) / 23) / 100, 3) for i in range(0, 24)}
+get_percent = lambda value: round(((20 * value) / 23) / 100, 3) if value < 23 else 0.2
 # Макс кол-во подстанций типа Б: 3
 # 15 МВт - Макс выход ЭС
 # 23 МВт - Макс мощность подстанции
 
 
 def get_out_power(value: float) -> float:
-    return value - value * percents.get(int(value), 0.2)
+    return value - value * get_percent(value)
 
 def get_input_power(value: float) -> float:
-    return value + value * percents.get(int(value), 0.2)
+    return value + value * get_percent(value)
 
 
 def get_max_diesel_power(value: float):
