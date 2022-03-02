7f6e821b932f2a2bf7042ac4aa7d11e7566a3633
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-02 16:11:15 +0700

    Add files via upload

--- topology.py
+++ topology.py
@@ -1,18 +1,24 @@
-objects = {"hA": 0, "hB": 1, "ss": 1, "ws": 0, "hosp": 0, "manuf": 0}  # кол-во объектов
-get_percent = lambda value: round(((20 * value) / 23) / 100, 3) if value < 23 else 0.2
 # Макс кол-во подстанций типа Б: 3
 # 15 МВт - Макс выход ЭС
 # 23 МВт - Макс мощность подстанции
+num_of_stationsA = 0
+num_of_stationsB = 1
+topology = [[[], [], []] * num_of_stationsA, [[], []] * num_of_stationsB]
+generators = {"solar": 1, "wind": 0}
+consumers = {"hA": 1, "hB": 0, "hosp": 0, "manuf": 0}
+
+get_percent = lambda value: round(((20 * value) / 23) / 100, 3) if value < 23 else 0.2
 
 
 def get_out_power(value: float) -> float:
     return value - value * get_percent(value)
 
+
 def get_input_power(value: float) -> float:
     return value + value * get_percent(value)
 
 
-def get_max_diesel_power(value: float):
+def get_max_diesel_power(value: float) -> float:
     max_value = 0
     for i in range(5, 0, -1):
         if get_out_power(value + i) <= 23:
@@ -23,3 +29,31 @@ def get_max_diesel_power(value: float):
             if get_out_power(i + value) <= 23:
                 return i
     return max_value
+
+
+obj_types = {
+    "solar": 15,  # солнечные электростанции
+    "wind": 15,  # ветровые электростанции
+    "houseA": -5,  # дом А
+    "houseB": -5,  # дом Б
+    "factory": -5  # заводы
+    # "hospital" -10  # больницы
+}
+line = ["houseB", "houseB", "houseB"]
+
+
+def get_consumption_of_line(line: list) -> float:
+    sum = 0
+    for obj in line:
+        sum += abs(obj_types.get(obj, 0))
+    return get_input_power(sum)
+
+
+def get_charge_of_line(line: list) -> float:
+    sum = 0
+    for obj in line:
+        sum += obj_types.get(obj, 0)
+    return get_out_power(sum)
+
+
+print(get_consumption_of_line(line))
