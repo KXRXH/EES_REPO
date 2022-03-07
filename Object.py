f49805fe1c3ff3015b7f2a53320b63e81bddb46b
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2022-03-07 13:03:15 +0700

    Graph class added

--- Object.py
+++ Object.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class Object(Enum):
+    # HouseB = (cost=10, qty=..., ...)
     def __init__(self, cost, qty, path, charge, prefix):
         # cost < 0 - Мы платим. для генераторов
         self.prefix = prefix
